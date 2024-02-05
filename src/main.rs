b541c487274e569c14c08ab381b20a0fe01fc135
Gordon Cheung <porkguy69@Gordons-Air.cable.virginm.net>
2024-02-05 20:00:05 +0000

    6

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ use mysql::*;
 
 //TODO: implement enecryption and hashing
 
+pub fn join_2D_vector(v: &Vec<Vec<&str>>) -> String {
+    let mut sub_vector = Vec::new();
+    for subvec in v.iter() {
+        sub_vector.push(subvec.join(":"));
+    }
+    sub_vector.join("\n").to_string()
+}
+
 pub fn load_password(website: String) -> Result<()> {
     let mut file = File::open("passwords.txt")?;
     let mut contents = String::new();
@@ -21,7 +29,21 @@ pub fn load_password(website: String) -> Result<()> {
     Ok(())
 }
 
-pub fn change_password(website: String) -> Result<()> {
+pub fn change_password(website: String, password: String) -> Result<()> {
+    let mut file = OpenOptions::new().write(true).read(true).open("passwords.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let lines = contents.split("\n");
+    let mut temp = Vec::new();
+    for line in lines {
+        let mut x = line.split(":").collect::<Vec<&str>>();
+        if x[0].to_lowercase() == website.trim().to_lowercase() {
+            x[1] = password.trim();
+        }
+        temp.push(x);
+    }
+    let mut file = File::create("passwords.txt");
+    file?.write(&join_2D_vector(&temp).into_bytes());
     Ok(())
 }
 
@@ -37,31 +59,31 @@ fn main() -> io::Result<()> {
     println!("2: Add new password");
     println!("3: Change password");
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
+    let _ = io::stdin().read_line(&mut buffer)?;
     let option = buffer.trim().parse::<u8>().unwrap();
     match option {
         1 => {
             println!("Please provide the website you want your password from: ");
             let mut website = String::new();
-            io::stdin().read_line(&mut website)?;
+            let _ = io::stdin().read_line(&mut website)?;
             let _ = load_password(website);
         },
         2 => {
             let mut website = String::new();
             let mut password = String::new();
             println!("Website name: ");
-            io::stdin().read_line(&mut website);
+            let _ = io::stdin().read_line(&mut website);
             println!("Password: ");
-            io::stdin().read_line(&mut password);
+            let _ = io::stdin().read_line(&mut password);
             let _ = create_password(website, password);
         },
         3 => {
             let mut website = String::new();
             let mut password = String::new();
             println!("Website name: ");
-            io::stdin().read_line(&mut website);
+            let _ = io::stdin().read_line(&mut website);
             println!("Password: ");
-            io::stdin().read_line(&mut password);
+            let _ = io::stdin().read_line(&mut password);
             let _ = change_password(website, password);
         },
         0_u8 | 4_u8..=u8::MAX => todo!(),
